3bd1fc696937d2617962449cd475dbab7169b445
YH-LEE21 <ksmwy9029@naver.com>
2022-05-09 21:01:24 +0900

    Docker

---
+++ vue3-homework/src/layouts/HeaderView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>여기는 헤더 페이지 입니다.</h1>
+</template>
---
+++ vue3-homework/src/views/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>할일 리스트</h3>
+    <input type="text" v-model="newTodo">
+    <button type="button" @click="addlist()">추가</button>
+    <hr>
+    <ul>
+      <li v-for="(n,index) in listzip" :key="index">
+        {{index+1}}. {{n}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      listzip : ["vue 공부하기","Spring 공부하기","게임하기"],
+      newTodo : ""
+    
+    }
+  },
+  methods: {
+    addlist(){
+      this.listzip.push(this.newTodo);
+      this.newTodo="";
+    }
+  },
+}
+</script>
